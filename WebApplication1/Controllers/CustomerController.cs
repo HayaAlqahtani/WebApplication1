a1d6bd786a0e2ab55c0becb4e226be52b87fe06e
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-04-16 13:40:17 +0300

    Add project files.

---
+++ WebApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomerController : Controller
+
+
+    {
+        public IActionResult Index()
+        {
+
+          List<Customer> customers = new List<Customer>
+           {
+                     new Customer { Id = 1, Name = "haya", Email ="haya@gmail.com"}
+           };
+            ViewData["data"] = customers;
+
+            return View();
+        }
+    }
+}
