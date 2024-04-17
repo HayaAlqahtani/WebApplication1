4e82710ebfd87e40fcb296de70b972c65c6e3c15
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-04-17 13:34:10 +0300

    handling and validation

--- WebApplication1/Controllers/CustomerController.cs
+++ WebApplication1/Controllers/CustomerController.cs
@@ -7,16 +7,38 @@ namespace WebApplication1.Controllers
 
 
     {
+        List<Customer> customers = new List<Customer>
+           {
+                     new Customer { Id = 1, Name = "haya", Email ="haya@gmail.com"},
+                     new Customer { Id = 2, Name = "amar", Email ="amar@gmail.com"},
+                     new Customer { Id = 3, Name = "mimi", Email ="mimi@gmail.com"}
+
+
+           };
         public IActionResult Index()
         {
 
-          List<Customer> customers = new List<Customer>
-           {
-                     new Customer { Id = 1, Name = "haya", Email ="haya@gmail.com"}
-           };
+
             ViewData["data"] = customers;
 
             return View();
         }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Register(AddCustomer form) { 
+        var name = form.Name;
+            var email = form.Email;
+            customers.Add(new Customer {  Id = 1, Name = name, Email = email });
+            return RedirectToAction("Index");
+        
+        
+        }
     }
+
 }
--- WebApplication1/Models/AddCustomer.cs
+++ WebApplication1/Models/AddCustomer.cs
@@ -1,11 +1,10 @@
 ﻿namespace WebApplication1.Models
 {
-    public class Customer
+    public class AddCustomer
     {
-        public int Id { get; set; } 
+      
         public string Name { get; set; }
         public string Email { get; set; }
+
     }
-
-
 }
